2c5abd6671ee2337875b1ff2dbb68f92441c31bf
Kristoffer Ley <kristoffer.ley@gmail.com>
2023-12-09 12:43:11 -0300

    Added ScoreTracker

---
+++ YouDigIt!/Assets/Scipts/GridCreatorScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCreatorScript : MonoBehaviour
+{
+    public GameObject dirt;
+    public GameObject grass;
+    public GameObject stone;
+    public GameObject gold;
+    public GameObject purpleGem;
+    public GameObject blueGem;
+    public GameObject greenGem;
+    public GameObject fuel;
+
+    void Start()
+    {        
+    }
+}
