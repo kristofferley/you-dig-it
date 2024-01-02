8ef0c6b71073eb349efc15188a55fc992079e8a7
Kristoffer Ley <kristoffer.ley@gmail.com>
2024-01-02 12:32:17 +0100

    Added colliders for objects

--- YouDigIt!/Assets/Scipts/PlayerManager.cs
+++ YouDigIt!/Assets/Scipts/PlayerManager.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rb;
+    public float moveSpeed;
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localScale = new Vector3(-2.25f, 2.25f, 1);
+            Vector2 movement = moveSpeed * Time.deltaTime * Vector2.left;
+            rb.MovePosition(rb.position + movement);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localScale = new Vector3(2.25f, 2.25f, 1);
+            Vector2 movement = moveSpeed * Time.deltaTime * Vector2.right;
+            rb.MovePosition(rb.position + movement);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector2 movement = moveSpeed * Time.deltaTime * Vector2.up;
+            rb.MovePosition(rb.position + movement);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector2 movement = moveSpeed * Time.deltaTime * Vector2.down;
+            rb.MovePosition(rb.position + movement);
+        }
     }
 }
